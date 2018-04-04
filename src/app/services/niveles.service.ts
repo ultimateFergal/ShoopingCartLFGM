fe3c993d1df451db0900da5a98642202b34b63d0
Luis Fernando Galarcio Marin <fernando.galarcio@gmail.com>
2018-04-04 10:37:26 -0500

    cgfhj

--- src/app/services/niveles.service.ts
+++ src/app/services/niveles.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/of';
-import { Subject } from '../../../LFGRM-EL-BARATON/node_modules/rxjs/Subject';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class NivelesService {
